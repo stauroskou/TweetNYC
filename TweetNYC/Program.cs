61d3697fe06906e26f6f3e04dc9b592cff6259fb
stavros koutsoukos <s.koutsoukos@vision-solutions.gr>
2022-05-20 18:50:24 +0300

    Credentials

--- TweetNYC/Program.cs
+++ TweetNYC/Program.cs
@@ -9,7 +9,8 @@ namespace TweetNYC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var userCredentials = new TwitterCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
+            var userClient = new TwitterClient(userCredentials);
         }
     }
 }
